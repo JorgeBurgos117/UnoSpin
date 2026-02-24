3647e65fc9e29e5519e0941be4c5bb8522f730fc
Jorge Burgos <134560494+JorgeBurgos117@users.noreply.github.com>
2026-02-24 16:22:23 -0700

    pnlConfigurarPartida
    Contenido y estructura básica de pnlConfigurarPartida

--- Presentacion/src/main/java/menuPrincipal/PnlConfigurarPartida.java
+++ Presentacion/src/main/java/menuPrincipal/PnlConfigurarPartida.java
@@ -7,14 +7,45 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import styles.CustomLabel;
+import styles.RoundTextfield;
 
 public class PnlConfigurarPartida extends JPanel {
 
     Style style = new Style();
-
+    
+    //Variables para lógica
+    
+    //Labels e inputs
+        //Contenedor
+    JPanel contenido = new JPanel();
+        //Titulo
+    CustomLabel lblTitulo = new CustomLabel("Configurar partida");
+        //1-Rango de cartas
+    CustomLabel lblRangoCartas = new CustomLabel("Rango de cartas");
+    CustomLabel lblMin1 = new CustomLabel("Mínimo: ");
+    RoundTextfield txfdMin1 = new RoundTextfield("min");
+    CustomLabel lblMax1 = new CustomLabel("Máximo: ");
+    RoundTextfield txfdMax1 = new RoundTextfield("max");
+        //2-Comodines
+    CustomLabel lblComodines = new CustomLabel("Número de comodiens");
+    CustomLabel lblMin2 = new CustomLabel("Mínimo: ");
+    RoundTextfield txfdMin2 = new RoundTextfield("min");
+    CustomLabel lblMax2 = new CustomLabel("Máximo: ");
+    RoundTextfield txfdMax2 = new RoundTextfield("max");
+        //3-Cartas de acción
+    CustomLabel lblCartasAccion = new CustomLabel("Número de cartas de acción: ");
+    CustomLabel lblMin3 = new CustomLabel("Mínimo: ");
+    RoundTextfield txfdMin3 = new RoundTextfield("min");
+    CustomLabel lblMax3 = new CustomLabel("Máximo: ");
+    RoundTextfield txfdMax3 = new RoundTextfield("max");
+        //4-Tiempo de turno
+    CustomLabel lblTiempoTurno = new CustomLabel("Tiempo de turno: ");
+    RoundTextfield txfdTiempo = new RoundTextfield("tiempo");
+    
     //Botones
-    RoundButton btnContinuar = new RoundButton("Crear partida", Color.yellow);
-    RoundButton btnCancelar = new RoundButton("Unirse a partida", Color.yellow);
+    RoundButton btnContinuar = new RoundButton("Continuar", Color.green);
+    RoundButton btnCancelar = new RoundButton("Cancelar", Color.red);
 
     public PnlConfigurarPartida() {
 
@@ -24,8 +55,35 @@ public class PnlConfigurarPartida extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         //Contenido
-        add(btnContinuar);
-        add(btnCancelar);
+        contenido.setLayout(new GridLayout(5, 5));
+            //Titulo
+        //contenido.add(lblConfigurarPartida);
+            //1-Rango
+        contenido.add(lblRangoCartas);
+        contenido.add(lblMin1);
+        contenido.add(txfdMin1);
+        contenido.add(lblMax1);
+        contenido.add(txfdMax1);
+            //2-Comodines
+        contenido.add(lblComodines);
+        contenido.add(lblMin2);
+        contenido.add(txfdMin2);
+        contenido.add(lblMax2);
+        contenido.add(txfdMax2);
+            //3-Cartas de acción
+        contenido.add(lblCartasAccion);
+        contenido.add(lblMin3);
+        contenido.add(txfdMin3);
+        contenido.add(lblMax3);
+        contenido.add(txfdMax3);
+            //4-Tiempo
+        contenido.add(lblTiempoTurno);
+        contenido.add(txfdTiempo);
+            //Botones
+        contenido.add(btnContinuar);
+        contenido.add(btnCancelar);
+        //Add
+        add(contenido);
 
         btnContinuar.addMouseListener(new MouseAdapter() {
             @Override
@@ -48,7 +106,7 @@ public class PnlConfigurarPartida extends JPanel {
     }
 
     void cancelar() {
-        System.out.println("Cancelando; regresando al menú porincipal");
+        System.out.println("Cancelando; regresando al menú principal");
     }
 
 }
